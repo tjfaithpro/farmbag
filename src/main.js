d06b9124c4dfa87f44a916a3e169b4bb1b4f6896
TJ FAITH <tjbenbiz@gmail.oom>
2020-05-25 19:18:34 -0700

    NJ

--- src/main.js
+++ src/main.js
@@ -11,7 +11,7 @@ Vue.use(VueAxios, axios)
 Vue.use(VueSession)
 
 // export const bus = new Vue();
-Vue.prototype.$hostname = 'http://localhost/MY_WEB_WORKS/farmbag_vue_cli/';
+Vue.prototype.$hostname = 'https://farmbag.com.ng/';
 window.$ = window.jQuery = require("jquery");
 window.$ = require('jquery') 
 // window.JQuery = require('jquery')
